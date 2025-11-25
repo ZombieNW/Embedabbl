14ae21125e2fe3633c24d99e721eb8ddb1bf2442
ZombieNW <59242209+ZombieNW@users.noreply.github.com>
2025-11-24 21:41:31 -0700

    Remove unused endpoints

--- src/routes/test/+page.svelte
+++
@@ -1,8 +0,0 @@
-<script>
-	import { cores } from '$lib/cores/registry';
-	import CoreLoader from '$lib/components/CoreLoader.svelte';
-
-	const coreData = cores['ruffle'];
-</script>
-
-<CoreLoader core={coreData} contentUrl={'https://embedabbl.zombienw.com/ruffle/test.swf'} />
